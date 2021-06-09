004e0ddc20ac66bf64dfe1ee7ff091061bea9989
Erizo <WhiteShadow4007@gmail.com>
2021-06-10 00:51:43 +0200

    small fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,35 @@
 #include<iostream>
 int main(int argc, char** args){
-    uint8_t type;
+    uint16_t type;
     int diameter;
     int radius;
-    std::cout << "What do you want to calculate?\n1-radius\n2-diameter\n3-circumference?";
+    std::cout << "What do you want to calculate?\n1-radius\n2-diameter\n3-circumference?" << std::endl;
     std::cin >> type ;
     switch(type){
         case 1:
-            std::cout << "what is the diameter?";
+            std::cout << "what is the radius?"<< std::endl;
             std::cin >> diameter;
             std::cout << diameter / 2;
             break;
         case 2:
-            std::cout <<"what is the radius?";
+            std::cout <<"what is the diameter?"<< std::endl;
             std::cin >> radius;
             std::cout << diameter * 2;
             break;
         case 3:
-            std::cout << "1-with diameter\n2-with radius";
+            std::cout << "1-with diameter\n2-with radius"<< std::endl;
             std::cin >> type;
-            
-            if(type == 1){
-                std::cout <<"what is the radius?";
-                int radius;
-                std::cout <<2 * 3.141 * radius;
-            }
-            if(type == 2){
-                std::cout << "what is the diameter?";
-                std::cin >> diameter;
-                std::cout <<3.141 * diameter;
+            switch(type){
+                case 1:
+                    std::cout <<"what is the radius?"<< std::endl;
+                    int radius;
+                    std::cout <<2 * 3.141 * radius;
+                    break;
+                case 2:
+                    std::cout << "what is the diameter?"<< std::endl;
+                    std::cin >> diameter;
+                    std::cout <<3.141 * diameter;
+                    break;
             }
             break;
     }
